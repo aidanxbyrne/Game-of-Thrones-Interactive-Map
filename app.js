e8bb9b0b3f0708ce0527e52195a7b1f7312b7034
Aidan Byrne <aidanbyrne142@gmail.com>
2021-08-06 23:25:09 +0100

    Side window refactoring
    - Refactored all content window code to JS
    - Removed unused CSS
    - Moved author information to ArticleData JS file

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
 
-console.log('Hello World')
-
-//REGIONS
-const beyond = { element: document.querySelector('#beyond'), article: document.querySelector('#article1')};
-const north = { element: document.querySelector('#north'), article: document.querySelector('#article2')};
-const riverlands = { element: document.querySelector('#riverlands'), article: document.querySelector('#article3')};
-const westlands = { element: document.querySelector('#westlands'), article: document.querySelector('#article4')};
-const stormlands = { element: document.querySelector('#stormlands'), article: document.querySelector('#article5')};
-const dorne = { element: document.querySelector('#dorne'), article: document.querySelector('#article6')};
+const beyond = document.querySelector('#beyond');
+const north = document.querySelector('#north');
+const riverlands = document.querySelector('#riverlands');
+const westlands = document.querySelector('#westlands');
+const stormlands = document.querySelector('#stormlands');
+const dorne = document.querySelector('#dorne');
 const regions = [ beyond, north, riverlands, westlands, stormlands, dorne];
 const articles = regions.map(article => article.article);
 
+//BUTTONS
 const aboutBtn = document.getElementById("authorInfo");
 const aboutSection = document.querySelector("#about");
 const allBtn = document.getElementById("openAll");
-const allArticles = document.querySelector("#allarticles");
-const closeBtn = document.querySelectorAll(".close");
+const closeBtn = document.querySelectorAll("#close");
 const randomBtn = document.querySelector("#randomBtn");
 
-//EVENT LISTENERS
-regions.forEach( ({element, article}) => {
-    element.addEventListener("mouseover", () => {
-        regionHover(element);
+//NEW STUFF
+const articleWindow = document.querySelector("#article-window");
+
+regions.forEach( (region) => {
+    region.addEventListener("mouseover", () => {
+        regionHover(region);
     });
-    element.addEventListener("mouseout", () => {
+    region.addEventListener("mouseout", () => {
         regionHover(null);
     });
-    element.addEventListener("click", () =>{
-        openInfo(article);
+    region.addEventListener("click", () =>{
+        openInfo(region);
     });
 });
 
 aboutBtn.addEventListener("click", () =>{
-    openInfo(aboutSection);
+    openAuthor();
 });
 allBtn.addEventListener("click", () =>{
-    openInfo(allarticles);
+    openAll();
 });
 randomBtn.addEventListener("click", () =>{
     openRandom();
@@ -52,44 +51,30 @@ function regionHover(region){
     }
 }
 
-function openInfo(article){
+function openInfo(region){
     hideAll();
-    getArticleData(article, article.id);
+    getArticleData(region);
     pageChange();
-    article.style.width = "40%";
+    toTop();
+    articleWindow.style.width = "40%";
 }
 
-//Hides all articles when user selects a different one.
-function hideAll(){
-    articles.forEach(article => {
-        article.style.width = "0%";
-    });
-    allArticles.style.width = "0%";
-    aboutSection.style.width = "0%";
-}
-
-function openRandom(){
-    let random = regions[Math.floor(Math.random()*6)].article;
-
+//Closes all articles when user clicks the close button.
+function closeInfo(){
     hideAll();
-    openInfo(random);
-}
+    document.getElementById("pageinfo").style.marginLeft = "-10px";
+    document.getElementById("westerosmap").style.width = "100%";
+    document.body.style.overflowY = "scroll";
+}	
 
-//Changes map and information positioning when articles are opened
-function pageChange(){
-    document.getElementById("pageinfo").style.marginLeft = "-1000px";
-    document.getElementById("westerosmap").style.float = "left";
-    document.getElementById("westerosmap").style.width = "60%";
-    document.body.style.overflow = "hidden";
-}
-
-function getArticleData(article, articleID){
+function getArticleData(region){
     let heading, title, author, date, imgUrl, imgAlt, imgCaption;
 
-    if(articles.includes(article)){
+    console.log(region);
+    if(regions.includes(region)){
         
         articleData.articles.forEach(art => {
-            if(articleID == art.id){
+            if(region.id == art.id){
                 heading = art.heading;
                 title = art.title;
                 author = art.author;
@@ -101,27 +86,120 @@ function getArticleData(article, articleID){
             }
         });
 
-        article.innerHTML = `
-            <h4 onClick="closeInfo();"><button class = "close" >X - Close</button></h4>
-                
+        articleWindow.innerHTML = `
             <div class="heading">
                 <h1>${heading}</h1>
+                <h3 id="close" onClick="closeInfo();"">X</h3>
             </div>
+
+            <div class="articleBody">
+                <h2>${title}</h2>
+
+                <div class="imgcaption">
+                    <p>By <b>${author} </b>on ${date}</p>
+                </div>	
                 
-            <h2>${title}</h2>
-            <div class="imgcaption">
-                <p>By <b>${author} </b>on ${date}</p>
-            </div>	
-            
-            <img src="${imgUrl}" alt="${imgAlt}" class="img">
-            
-            <div class="imgcaption">
-                <p><em>${imgCaption}</em></p>
-            </div>
-            <br>
-            <div>
-                ${articleBody};
+                <img src="${imgUrl}" alt="${imgAlt}" class="img">
+                
+                <div class="imgcaption">
+                    <p><em>${imgCaption}</em></p>
+                </div>
+
+                <div>
+                    ${articleBody}
+                </div>
             </div>
         `; 
     }   
+}
+
+//Hides all articles when user selects a different one.
+function hideAll(){
+    articleWindow.style.width = "0%";
+}
+
+//Changes map and information positioning when articles are opened
+function pageChange(){
+    document.getElementById("pageinfo").style.marginLeft = "-1000px";
+    document.getElementById("westerosmap").style.float = "left";
+    document.getElementById("westerosmap").style.width = "60%";
+    document.body.style.overflow = "hidden";
+} 
+
+//Resets page to top if user has scrolled down and clicks on one of the buttons
+function toTop(){
+    scroll(0,0)
+}
+
+function openAll(){
+    hideAll();
+    pageChange();
+    articleWindow.style.width = "40%";
+
+    let articleDetails = '';
+
+    articleData.articles.forEach(article => {
+        articleDetails += `
+            <div class="articlePreview">
+                <h2 onClick="hideAll(); pageChange(); openInfo(${article.id});">${article.title}</h2>
+                <div class="imgcaption">
+                    <p>By <b>${article.author}</b>on ${article.date}</p>
+                </div>
+                <img src="${article.imgUrl}" alt="${article.imgAlt}" class="imgsmall">
+                <p>${article.description}</p>
+                <br>
+            </div>
+        `;
+    });
+
+    articleWindow.innerHTML = `
+        <div class="heading">
+        <h1>All Articles</h1>
+        <h3 id="close" onClick="closeInfo();"">X</h3>
+        </div>
+
+        <div class="articleBody">
+            ${articleDetails}
+        </div>
+    `
+}
+
+function openRandom(){
+    let random = regions[Math.floor(Math.random()*6)];
+
+    hideAll();
+    openInfo(random);
+}
+
+function openAuthor(){
+    hideAll();
+    pageChange();
+    articleWindow.style.width = "40%";
+
+    let authorDetails = '';
+
+    articleData.authors.forEach(author => {
+        authorDetails += `
+            <div class="authors authorDetail">
+                <div class="authorimg">
+                    <img src="${author.authorImgUrl}" alt="${author.authorImgAlt}">
+                </div>
+                <div class="authorabout">
+                    <h2>${author.name}</h2>
+                    <p>${author.about}</p>
+                </div>
+            </div>
+        `;
+    });
+
+    articleWindow.innerHTML = `
+        <div class="heading">
+        <h1>About the Authors</h1>
+        <h3 id="close" onClick="closeInfo();"">X</h3>
+        </div>
+
+        <div class="articleBody">
+            ${authorDetails}
+        </div>
+    `
 }
